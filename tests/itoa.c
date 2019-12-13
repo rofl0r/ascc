08aa5177f4b622e9a544090e7aca7071304623cf
rofl0r <rofl0r@users.noreply.github.com>
2019-12-13 02:53:56 +0000

    add itoa test

---
+++ tests/itoa.c
@@ -0,0 +1,38 @@
+void puts(char*) {}
+void itoa(int number, char* buffer) {
+	int lentest, len = 0, i, start;
+
+	if(number >> 31) {
+		number = number * -1;
+		buffer[0] = '-';
+		start = 1;
+	} else
+		start = 0;
+
+	lentest = number;
+	do {
+		len++;
+		lentest /= 10;
+	} while(lentest);
+	buffer[start+len] = 0;
+	do {
+		i = number % 10;
+		buffer[start+len - 1] = '0' + i;
+		number -= i;
+		len -= 1;
+		number /= 10;
+	} while (number);
+}
+
+int main() {
+	char buf[128];
+	itoa(-1337, buf);
+	puts(buf);
+	itoa(0, buf);
+	puts(buf);
+	itoa(100000, buf);
+	puts(buf);
+	itoa(-5653452, buf);
+	puts(buf);
+	return 1337/70;
+}
